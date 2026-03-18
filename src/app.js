5a9a7ab62370d5f2fdec8534a6901c3288d1e6b1
Paul Ciurean <paul.ciurean@rebura.com>
2026-03-18 16:23:05 +0000

    prod env

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ module.exports = function() {
 
   /* istanbul ignore next */
   app.get('/hi', (req, res) => {
-    res.status(200).send('Hi! Paul from Rebura here!');
+    res.status(200).send('Hi! Paul from Rebura here! This is the prod env!');
   });
 
   return app;
